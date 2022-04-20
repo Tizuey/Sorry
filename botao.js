247001ff7cdd5a0cc1571a0d023d7421d6963737
Tizuey <tizuey00@gmail.com>
2022-04-20 18:03:29 -0300

    eu consegui

---
+++ botao.js
@@ -0,0 +1,15 @@
+
+// // conectando os id a variáveis
+// const reposta = document.getElementById('resposta');
+// const modal_container = document.getElementById('modal_container');
+// const close = document.getElementById('close');
+
+// //abrir modal
+// open.addEventListener('click',() => {
+//     modal_container.classList.add('show');
+// });
+
+// // fechar modal
+// closed.addEventListener('click',() => {
+//     modal_container.classList.remove('show');
+// });
